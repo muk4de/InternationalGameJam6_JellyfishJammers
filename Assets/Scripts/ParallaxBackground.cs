12cf58834ce356fd33cbfcb056b2d0c42533a139
muk4de <moritashouta0@gmail.com>
2025-12-15 22:15:49 +0900

    add: cinamachine and dotween asset.  test scene

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [Header("Settings")]
+    public GameObject cam;
+
+    [RangeAttribute(0f, 1f)]public float parallaxEffect;
+
+    public const float farMax = 50;
+
+    private float length;
+    private float startpos;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    { 
+        float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+
+    private void OnValidate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, farMax * parallaxEffect);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    // inspector
+    [Header("Player Movement")]
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float accelerationTime = 0.5f; // 最高速度に達するまでの時間（秒）
+    [SerializeField] float decelerationTime = 0.5f; // 停止するまでの時間（秒）
+
+    [Header("Input system")]
+    [SerializeField] InputAction moveAction;
+
+    // private
+    Rigidbody2D rb;
+    Vector2 moveInput;
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+        Flip();
+    }
+
+    void FixedUpdate()
+    {
+        Swim();
+    }
+
+    void Swim()
+    {
+        Vector2 targetVelocity = moveInput * moveSpeed;
+
+        float accelerationRate;
+
+        if (moveInput.sqrMagnitude > 0.01f)
+        {
+            accelerationRate = moveSpeed / Mathf.Max(accelerationTime, 0.01f);
+        }
+        else
+        {
+            accelerationRate = moveSpeed / Mathf.Max(decelerationTime, 0.01f);
+        }
+
+        rb.linearVelocity = Vector2.MoveTowards(
+            rb.linearVelocity,
+            targetVelocity,
+            accelerationRate * Time.fixedDeltaTime
+        );
+    }
+
+    void Flip()
+    {
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+}
